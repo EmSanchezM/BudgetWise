d12060855ff64b34c25525d797ae3bcb97383446
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2024-03-27 09:36:54 -0600

    feat: configuration tailwindcss

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,14 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
+    <main class="container flex items-center p-4 mx-auto min-h-screen justify-center">
+      <h1 class="text-center bg-blue-500">Hi 👋</h1>
+      <p class="flex justify-center items-center mx-auto">
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </p>
-    </>
+    </main>
   );
 });
 

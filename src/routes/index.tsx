fdf4ee6c80694cfd3b7ab50ce4c35960219e2b84
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2024-03-27 08:09:08 -0600

    feat: add description app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,11 +15,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "BudgetWise App",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "A personal finance app that tracks expenses, creates budgets and provides money-saving tips",
     },
   ],
 };
